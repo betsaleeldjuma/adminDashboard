56a6bb1ba08a2ed695f6d05ddb4a42fcb14a77e2
betsaleeldjuma <djumabestaleel@gmail.com>
2026-01-06 22:59:53 +0200

    Update Login

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -18,13 +18,13 @@ const Login = () => {
     }
     
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-4 w-screen h-screen text-[#B4A5A5]">
-      <div className="flex flex-col justify-center items-center gap-4 w-[80%] h-[60%] bg-[#301B3F] rounded-lg shadow-lg">
+    <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-4 w-screen h-screen">
+      <div className="flex flex-col justify-center items-center gap-4 w-[80%] h-[60%] bg-[#8E1616] rounded-lg shadow-lg">
         <label className="text-xl font-bold">Name:</label>
         <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="border p-2 w-[40%] rounded-sm"/>
         <label className="text-xl font-bold">Password:</label>
         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="border p-2 w-[40%] rounded-sm"/>
-        <button type="submit" className="bg-[#3C415C] p-2 rounded-lg shadow-lg w-[40%] text-white">Login</button>
+        <button type="submit" className="bg-[#D84040] p-2 rounded-lg  hover:shadow-[0_0_3px_#EEEEEE] w-[40%]">Login</button>
       </div>
         
     </form>
