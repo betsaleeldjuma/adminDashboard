6266e5aff2b1f7067bcf808d0b57544b48596084
betsaleeldjuma <djumabestaleel@gmail.com>
2026-01-08 22:40:10 +0200

    Update Products

--- src/routes/Products.tsx
+++ src/routes/Products.tsx
@@ -12,10 +12,14 @@ const Products = () => {
   if(!data) return <div className="flex justify-center items-center w-screen h-screen"><EmptyState /></div>;
 
   return (
-    <div className="flex flex-col gap-8">
-      {data.map((product) => <div key={product.id} className="bg-[#8E1616] ">
-        {product.title} - ${product.price}
-      </div>)}
+    <div className="flex flex-col gap-4 p-5">
+      <h1 className="font-extrabold text-2xl lg:text-4xl">PRODUCTS</h1>
+      <div className="flex flex-col gap-4">
+        {data.map((product) => <div key={product.id} className="bg-[#8E1616] flex justify-evenly rounded-lg p-4">
+          <h1 className="font-bold lg:text-2xl">{product.title}</h1> 
+          <p className="lg:text-2xl">${product.price}</p>
+        </div>)}
+      </div>
     </div>
   )
 }
